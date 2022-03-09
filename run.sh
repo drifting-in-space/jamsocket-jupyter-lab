1ed463c3b79b852185b98cdc9c14370a0939bfe6
Taylor Baldwin <taylorbaldwin@gmail.com>
2022-03-09 16:36:54 -0500

    fix notebook dir copy bug and add some helpful jupyter settings to run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,10 @@
 jupyter-lab --generate-config
 echo "c.ServerApp.token = '$JAMSOCKET_JUPYTER_TOKEN'" >> ~/.jupyter/jupyter_lab_config.py
 
+# automatically redirect / to a notebook by uncommenting and editing the following line:
+# echo "c.ServerApp.default_url = '/notebooks/[YOUR NOTEBOOK HERE].ipynb'" >> ~/.jupyter/jupyter_notebook_config.py
+
+# to embed the jupyter notebook in an iframe, uncomment the following line and edit your domain
+# echo "c.ServerApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors \'self\' [YOUR DOMAIN];'}}" >> ~/.jupyter/jupyter_notebook_config.py
+
 jupyter-lab --collaborative --ip 0.0.0.0 --no-browser
