f33b7ac82d355c1797ee70852b5a04e1e47e8cfb
Taylor Baldwin <taylorbaldwin@gmail.com>
2022-08-04 19:22:49 +0000

    remove default_url config since it doesnt seem to be working

--- run.sh
+++ run.sh
@@ -8,10 +8,6 @@ set -e
 JUPYTER_LAB_PASSWORD=""
 echo "c.ServerApp.token = '$JUPYTER_LAB_PASSWORD'" >> ~/.jupyter/jupyter_lab_config.py
 
-# provide the notebook you want to start Jupyter with on the following line:
-NOTEBOOK="hello-tmpenv.ipynb"
-echo "c.ServerApp.default_url = '/lab/tree/$NOTEBOOK'" >> ~/.jupyter/jupyter_lab_config.py
-
 # to embed jupyter lab in an iframe, uncomment the following line and edit your domain
 # echo "c.ServerApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors \'self\' [YOUR DOMAIN];'}}" >> ~/.jupyter/jupyter_lab_config.py
 
