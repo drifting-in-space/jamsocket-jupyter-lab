48233a7a91463d0c0c57415a4d08d87e9a513523
Taylor Baldwin <taylorbaldwin@gmail.com>
2022-08-04 19:19:22 +0000

    listen on $PORT - per comments

--- run.sh
+++ run.sh
@@ -15,4 +15,4 @@ echo "c.ServerApp.default_url = '/lab/tree/$NOTEBOOK'" >> ~/.jupyter/jupyter_lab
 # to embed jupyter lab in an iframe, uncomment the following line and edit your domain
 # echo "c.ServerApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors \'self\' [YOUR DOMAIN];'}}" >> ~/.jupyter/jupyter_lab_config.py
 
-/home/jupyter/notebook-env/bin/jupyter-lab --collaborative --ip 0.0.0.0 --port 8080 --no-browser
+/home/jupyter/notebook-env/bin/jupyter-lab --collaborative --ip 0.0.0.0 --port $PORT --no-browser
